a14341813ba41e750570b394dc8cbc19346a77c8
Raica Kathleen Perez <146512506+raicsp@users.noreply.github.com>
2024-12-08 18:17:43 +0800

    Changed port to 4000

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 app.use(express.json({ limit: '50mb' })); 
 app.use(cors()); 
 
